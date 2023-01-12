4e6bd3d644a111366f6d8be843f34c9cd420c872
thai <maot.tm@gmail.com>
2023-01-12 23:56:27 +0000

    stream wav files

--- src/source/wav_loader.rs
+++ src/source/wav_loader.rs
@@ -2,9 +2,10 @@ use anyhow::Result;
 use bevy::asset::{AssetLoader, LoadContext, LoadedAsset};
 use bevy::utils::BoxedFuture;
 use kira::sound::static_sound::{StaticSoundData, StaticSoundSettings};
+use kira::sound::streaming::{StreamingSoundData, StreamingSoundSettings};
 use std::io::Cursor;
 
-use crate::source::AudioSource;
+use crate::source::{AudioSource, AudioStreamingSource};
 
 #[derive(Default)]
 pub struct WavLoader;
@@ -25,6 +26,34 @@ impl AssetLoader for WavLoader {
                 StaticSoundSettings::default(),
             )?;
             load_context.set_default_asset(LoadedAsset::new(AudioSource { sound }));
+            Ok(())
+        })
+    }
+
+    fn extensions(&self) -> &[&str] {
+        &["wav"]
+    }
+}
+
+#[derive(Default)]
+pub struct StreamingWavLoader;
+
+impl AssetLoader for StreamingWavLoader {
+    fn load<'a>(
+        &'a self,
+        bytes: &'a [u8],
+        load_context: &'a mut LoadContext,
+    ) -> BoxedFuture<'a, Result<()>> {
+        Box::pin(async move {
+            let mut sound_bytes = vec![];
+            for byte in bytes {
+                sound_bytes.push(*byte);
+            }
+            let sound = StreamingSoundData::from_cursor(
+                Cursor::new(sound_bytes),
+                StreamingSoundSettings::default(),
+            )?;
+            load_context.set_default_asset(LoadedAsset::new(AudioStreamingSource { sound }));
             Ok(())
         })
     }
