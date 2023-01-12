e9f21bbc8c626f6579e3689a9d3808bf921bbffc
thai <maot.tm@gmail.com>
2023-01-12 23:50:14 +0000

    start stream example

---
+++ examples/stream.rs
@@ -0,0 +1,18 @@
+use bevy::prelude::*;
+use bevy_kira_audio::prelude::*;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(AudioPlugin)
+        .add_startup_system(play_loop)
+        .run()
+}
+
+// `Audio` is an alias for `AudioChannel<MainTrack>`, which is the default channel added by the audio plugin
+// See the `custom_channel` example to add your own audio channels
+fn play_loop(asset_server: Res<AssetServer>, audio: Res<Audio>) {
+    audio
+        .play_stream(asset_server.load("sounds/loop.ogg"))
+        .looped();
+}
